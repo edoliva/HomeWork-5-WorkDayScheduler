21bb91406f0de51c8e966865d88a186e630a42d4
Ed Oliva <edmond.oliva@gmail.com>
2021-03-31 20:24:50 -0400

    for loop updated, and README started

--- Assets/JS/script.js
+++ Assets/JS/script.js
@@ -3,114 +3,167 @@ var timeDisplayEl = $('#time-display');
 
 // handle displaying the time
 function displayTime() {
-    var rightNow = moment().format('MMM DD, YYYY h:m a');
+    var rightNow = moment().format('MMM DD, YYYY hh:mm a');
     timeDisplayEl.text(rightNow);
   }
 setInterval(displayTime, 1000);
 
-// row 1
-var localtask0 = localStorage.getItem("task0");
-var submitButton0 = document.querySelector("#submit-button0");
-var doneButton0 = document.querySelector("#done-button0"); 
-var lastTaskSpan0 = document.querySelector("#taskdisplay0");
-var task0 = document.querySelector("#task0");
-var row0 = document.querySelector("#row0"); 
 
-console.log(localtask0);
-if (localtask0 != null) {
-  task0.setAttribute("style", "display: none;");
-  lastTaskSpan0.setAttribute("style", "display: inline-block;");
-  lastTaskSpan0.textContent = localtask0;
-  submitButton0.setAttribute("style", "display: none;");
-  doneButton0.setAttribute("style", "display: inline-block;");
-} else {doneButton0.setAttribute("style", "display: none;");}
+
+// row ALL
+var localtaski = localStorage.getItem("task"+i);
+var submitButtoni = document.querySelector("#submit-button"+i);
+var doneButtoni = document.querySelector("#done-button"+i); 
+var lastTaskSpani = document.querySelector("#taskdisplay"+i);
+var taski = document.querySelector("#task"+i);
+var rowi = document.querySelector("#row"+i); 
+
+for (var i = 0; i <=8; i++) {
+  localtaski = localStorage.getItem("task"+i);
+  submitButtoni = document.querySelector("#submit-button"+i);
+  doneButtoni = document.querySelector("#done-button"+i); 
+  lastTaskSpani = document.querySelector("#taskdisplay"+i);
+  taski = document.querySelector("#task"+i);
+  rowi = document.querySelector("#row"+i); 
+
+console.log(localtaski);
+if (localtaski != null) {
+  taski.setAttribute("style", "display: none;");
+  lastTaskSpani.setAttribute("style", "display: inline-block;");
+  lastTaskSpani.textContent = localtaski;
+  submitButtoni.setAttribute("style", "display: none;");
+  doneButtoni.setAttribute("style", "display: inline-block;");
+} else {doneButtoni.setAttribute("style", "display: none;");}
    
-submitButton0.addEventListener("click", function(event) {  
-  var task0 = document.querySelector("#task0").value.trim();
-  if (task0 === "") {
+submitANDdone (i);
+
+}
+
+function submitANDdone (i) { 
+  localtaski = localStorage.getItem("task"+i);
+  submitButtoni = document.querySelector("#submit-button"+i);
+  doneButtoni = document.querySelector("#done-button"+i); 
+  lastTaskSpani = document.querySelector("#taskdisplay"+i);
+  taski = document.querySelector("#task"+i);
+  rowi = document.querySelector("#row"+i); 
+
+submitButtoni.addEventListener("click", function(event) {  
+  var taski = document.querySelector("#task"+i).value.trim();
+  if (taski === "") {
     alert("Enter SOMETHING!");
     return;
   }
-  localStorage.setItem("task0", task0);
-  lastTaskSpan0.textContent = task0;
-  task0 = document.querySelector("#task0");
-  task0.setAttribute("style", "display: none;");
-  lastTaskSpan0.setAttribute("style", "display: inline-block;");
-  submitButton0.setAttribute("style", "display: none;");
-  doneButton0.setAttribute("style", "display: inline-block;");
+  localStorage.setItem("task"+i, taski);
+  lastTaskSpani.textContent = taski;
+  taski = document.querySelector("#task"+i);
+  taski.setAttribute("style", "display: none;");
+  lastTaskSpani.setAttribute("style", "display: inline-block;");
+  submitButtoni.setAttribute("style", "display: none;");
+  doneButtoni.setAttribute("style", "display: inline-block;");
+  location.reload();
 });
 
-doneButton0.addEventListener("click", function(event) {  
-  localStorage.removeItem("task0");
-  task0 = document.querySelector("#task0");
-  task0.setAttribute("style", "display: inline-block;");
-  lastTaskSpan0.setAttribute("style", "display: none;");
-  submitButton0.setAttribute("style", "display: inline-block;");
-  doneButton0.setAttribute("style", "display: none;");
+doneButtoni.addEventListener("click", function(event) {  
+  localStorage.removeItem("task"+i);
+  taski = document.querySelector("#task"+i);
+  taski.setAttribute("style", "display: inline-block;");
+  lastTaskSpani.setAttribute("style", "display: none;");
+  submitButtoni.setAttribute("style", "display: inline-block;");
+  doneButtoni.setAttribute("style", "display: none;");
   location.reload()
 });
+};
+
+
+// row 1
+// var localtaski = localStorage.getItem("taski");
+// var submitButtoni = document.querySelector("#submit-buttoni");
+// var doneButtoni = document.querySelector("#done-buttoni"); 
+// var lastTaskSpani = document.querySelector("#taskdisplayi");
+// var taski = document.querySelector("#taski");
+// var rowi = document.querySelector("#rowi"); 
+
+// console.log(localtaski);
+// if (localtaski != null) {
+//   taski.setAttribute("style", "display: none;");
+//   lastTaskSpani.setAttribute("style", "display: inline-block;");
+//   lastTaskSpani.textContent = localtaski;
+//   submitButtoni.setAttribute("style", "display: none;");
+//   doneButtoni.setAttribute("style", "display: inline-block;");
+// } else {doneButtoni.setAttribute("style", "display: none;");}
+   
+// submitButtoni.addEventListener("click", function(event) {  
+//   var taski = document.querySelector("#taski").value.trim();
+//   if (taski === "") {
+//     alert("Enter SOMETHING!");
+//     return;
+//   }
+//   localStorage.setItem("taski", taski);
+//   lastTaskSpani.textContent = taski;
+//   taski = document.querySelector("#taski");
+//   taski.setAttribute("style", "display: none;");
+//   lastTaskSpani.setAttribute("style", "display: inline-block;");
+//   submitButtoni.setAttribute("style", "display: none;");
+//   doneButtoni.setAttribute("style", "display: inline-block;");
+// });
+
+// doneButtoni.addEventListener("click", function(event) {  
+//   localStorage.removeItem("taski");
+//   taski = document.querySelector("#taski");
+//   taski.setAttribute("style", "display: inline-block;");
+//   lastTaskSpani.setAttribute("style", "display: none;");
+//   submitButtoni.setAttribute("style", "display: inline-block;");
+//   doneButtoni.setAttribute("style", "display: none;");
+//   location.reload()
+// });
 
 
 // row 2
-var localtask1 = localStorage.getItem("task1");
-var submitButton1 = document.querySelector("#submit-button1");
-var doneButton1 = document.querySelector("#done-button1"); 
-var lastTaskSpan1 = document.querySelector("#taskdisplay1");
-var task1 = document.querySelector("#task1");
+// var localtask1 = localStorage.getItem("task1");
+// var submitButton1 = document.querySelector("#submit-button1");
+// var doneButton1 = document.querySelector("#done-button1"); 
+// var lastTaskSpan1 = document.querySelector("#taskdisplay1");
+// var task1 = document.querySelector("#task1");
 
-console.log(localtask1);
-if (localtask1 != null) {
-  task1.setAttribute("style", "display: none;");
-  lastTaskSpan1.setAttribute("style", "display: inline-block;");
-  lastTaskSpan1.textContent = localtask1;
-  submitButton1.setAttribute("style", "display: none;");
-  doneButton1.setAttribute("style", "display: inline-block;");
-} else {doneButton1.setAttribute("style", "display: none;");}
+// console.log(localtask1);
+// if (localtask1 != null) {
+//   task1.setAttribute("style", "display: none;");
+//   lastTaskSpan1.setAttribute("style", "display: inline-block;");
+//   lastTaskSpan1.textContent = localtask1;
+//   submitButton1.setAttribute("style", "display: none;");
+//   doneButton1.setAttribute("style", "display: inline-block;");
+// } else {doneButton1.setAttribute("style", "display: none;");}
    
-submitButton1.addEventListener("click", function(event) {  
-  var task1 = document.querySelector("#task1").value.trim();
-  if (task1 === "") {
-    alert("Enter SOMETHING!");
-    return;
-  }
-  localStorage.setItem("task1", task1);
-  lastTaskSpan1.textContent = task1;
-  task1 = document.querySelector("#task1");
-  task1.setAttribute("style", "display: none;");
-  lastTaskSpan1.setAttribute("style", "display: inline-block;");
-  submitButton1.setAttribute("style", "display: none;");
-  doneButton1.setAttribute("style", "display: inline-block;");
-});
+// submitButton1.addEventListener("click", function(event) {  
+//   var task1 = document.querySelector("#task1").value.trim();
+//   if (task1 === "") {
+//     alert("Enter SOMETHING!");
+//     return;
+//   }
+//   localStorage.setItem("task1", task1);
+//   lastTaskSpan1.textContent = task1;
+//   task1 = document.querySelector("#task1");
+//   task1.setAttribute("style", "display: none;");
+//   lastTaskSpan1.setAttribute("style", "display: inline-block;");
+//   submitButton1.setAttribute("style", "display: none;");
+//   doneButton1.setAttribute("style", "display: inline-block;");
+// });
 
-doneButton1.addEventListener("click", function(event) {  
-  localStorage.removeItem("task1");
-  task1 = document.querySelector("#task1");
-  task1.setAttribute("style", "display: inline-block;");
-  lastTaskSpan1.setAttribute("style", "display: none;");
-  submitButton1.setAttribute("style", "display: inline-block;");
-  doneButton1.setAttribute("style", "display: none;");
-  location.reload()
-});
-
-// if (hr >= startTime+1) {
-//   row1.setAttribute("style", "background-color: red;");
-// } else {row1.setAttribute("style", "background-color: green;");} 
+// doneButton1.addEventListener("click", function(event) {  
+//   localStorage.removeItem("task1");
+//   task1 = document.querySelector("#task1");
+//   task1.setAttribute("style", "display: inline-block;");
+//   lastTaskSpan1.setAttribute("style", "display: none;");
+//   submitButton1.setAttribute("style", "display: inline-block;");
+//   doneButton1.setAttribute("style", "display: none;");
+//   location.reload()
+// });
 
 
-
-
-
-
-// When the user scrolls the page, execute myFunction
+// pin time to top
 window.onscroll = function() {myFunction()};
-
-// Get the header
 var header = document.getElementById("myHeader");
-
-// Get the offset position of the navbar
 var sticky = header.offsetTop;
-
-// Add the sticky class to the header when you reach its scroll position. Remove "sticky" when you leave the scroll position
 function myFunction() {
   if (window.pageYOffset > sticky) {
     header.classList.add("sticky");
@@ -119,12 +172,16 @@ function myFunction() {
   }
 }
 
+// color rows
 var hr = (new Date()).getHours(); 
 var startTime = 9;
-
 for (var i = 0; i <= 8; i++) {
   var rowi = document.querySelector("#row"+i); 
-  if (hr >= startTime+i && hr < startTime+i+1) {rowi.setAttribute("style", "background-color: red;");}
-  else if  (hr < startTime+i+1) {rowi.setAttribute("style", "background-color: green;");} 
-  else {rowi.setAttribute("style", "background-color: grey;");}
+  if (hr >= startTime+i && hr < startTime+i+1) {
+    rowi.setAttribute("style", "background-color: #ff6961;");
+  } else if (hr < startTime+i+1) {
+    rowi.setAttribute("style", "background-color: #77dd77;");
+  } else {rowi.setAttribute("style", "background-color: #d3d3d3;");
+  }
 }
+
--- README.md
+++ README.md
@@ -1,1 +1,51 @@
 # HomeWork-5-WorkDayScheduler
+ 
+## HW 5 Description
+This webpage was created so a user could enter in a task they would like completed at some hour of the work day. Color prompts allow the user to better manage their tasks so they know what is past-due. 
+
+They can save the task, by clicking the SAVE button, and it will store locally so it shows up each time they open the webpage.
+
+Once the task it done, they can clear it from local storage but clicking the DONE button. The can then enter a new task and repeat the SAVE process.  
+ 
+## Table of Contents
+- [Installation](#installation)
+- [Criteria Checks](#criteria)
+- [Usage and Coding](#Usage/coding)
+- [Screenshots](#screenshots)
+- [Credits](#credits)
+- [License](#license)
+## Installation
+Clone or download this repository on GitHub then open "edoliva.github.io/HomeWork-5-WorkDayScheduler/index.html" in a browser.  
+## Criteria
+- The user must enter some text in the task box or they will receive an error when they try press the SAVE button for that time slot.
+- The user can leave a time slot open without receiving an error alert.   
+- If the user would like to clear the time slot the will press the DONE button. 
+## Usage/coding:
+- This HTML, CSS and JS were written to allow for a clean user experience. The general logic is as follows:
+   - The top of the webpage shows the date and current time. As the user scrolls down the Date/Time header will stick to the top of the page.
+   - Each row represents one hour of a standrd work day. As the hours pass, the row will turn grey; when in the current hour, the row will turn red; and when the hour is in the future the row will turn green. 
+   - The middle section of each row is an open text box for the user to type in the task. Only when they hit SAVE will the task save to local storage. The SAVE button will switch to "display:none" and the DONE button will switch to "display:inline-block". The input box will also "display:none" and the locally stored task will "display:inline-block".
+   - When the user clicks the DONE button, all of these styling displays will reverse so the user is only presented with the SAVE button and the input box.
+ 
+Screenshots:
+-
+![Header](Assets/Images/InfoBox.png)
+-
+![TimeBoxStick](Assets/Images/QuizQuestionBox.png)
+-
+![BlankInput](Assets/Images/FinalScoreBox.png)
+-
+![TextInInput](Assets/Images/HighScoreBox.png)
+-
+If user leaves answer blank:
+-
+![NoTaskError](Assets/Images/NoAnswerError.png)
+-
+ 
+## Credits
+Shoutout to our instructor Anthony and TA Tom! As well as everyone at Trilogy Education for making the content and grading our assignments.
+ 
+## License
+© 2021 Edmond Oliva.
+Confidential and Proprietary. All Rights Reserved.
+---
